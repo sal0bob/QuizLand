9c5d4ed21d75a85ffa8423bb6eed399f2aaedfdc
sal0bob <urao05092009@gmail.com>
2026-04-08 20:51:52 +0300

    Initial comit

--- app/src/main/java/com/example/test/QuizActivity.java
+++ app/src/main/java/com/example/test/QuizActivity.java
@@ -1,8 +1,6 @@
 package com.example.test;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,18 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    
+public class QuizActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void goTestBtn(View view) {
-        Intent intent = new Intent(this, QuizActivity.class);
+        setContentView(R.layout.activity_quiz);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 }
